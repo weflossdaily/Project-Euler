f653894e22ae8b1eebaa8241de22796f81df2c0b
weflossdaily <weflossdaily@gmail.com>
2014-05-20 17:06:49 -0400

    Update 094.py
    note for different area expression. will it help to remember (i+1) for later's (i-1)?

--- 094.py
+++ 094.py
@@ -7,6 +7,7 @@ print 'going'
 while perimeter<1000000000:
     # this area expression can probably get quicker since last iteration we already did some of the work
     area = .25*math.sqrt(i*(i*(i*(3*i+4)-2)-4)-1)
+    # area = .25*math.sqrt(((i+1)**2)*(i-1)*perimeter)
     
     # is the area integral?
     if area == math.floor(area):
