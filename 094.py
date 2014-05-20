286ae38c991ef833df6af8513c18948a2afc523c
weflossdaily <weflossdaily@gmail.com>
2014-05-19 23:10:20 -0400

    Update 094.py
    Probably faster, though I think it can be faster still

--- 094.py
+++ 094.py
@@ -2,13 +2,12 @@ import math
 
 sum = 0
 i = 1
-
-while 3*i+1<10000000001:
-    a=i 
-    b=i 
-    c=i+1
-    area = .25*math.sqrt((-a+b+c)*(a-b+c)*(a+b-c)*(a+b+c))
+perimeter = 3*i+1
+while perimeter<10000000001:
+    # this area expression can probably get quicker since last iteration we already did some of the work
+    area = .25*math.sqrt(i*(i*(i*(3*i+4)-2)-4)-1)
     if area == math.floor(area):
-        sum+=(a+b+c)
+        sum+=(perimeter)
     i+=1
+    perimeter = 3*i+1
 print str(sum)
