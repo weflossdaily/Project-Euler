1857a4aab22a757e52bc5c884724401e1bde14e6
weflossdaily <weflossdaily@gmail.com>
2014-05-19 20:13:34 -0400

    Update 045.py
    switched gears

--- 045.py
+++ 045.py
@@ -2,18 +2,7 @@
 import sys
 import math
 
-# def isEven(n):
-# 	return n % 2 == 0
-
-# def isDivisibleByThree(n):
-# 	return n % 3 == 0
-
 def isTriangular(n):
-	# twoN = 2 * n
-	# squareRoot = math.sqrt(twoN)
-	# low = math.floor(squareRoot)
-	# if low * low + low == twoN:
-	# 	print str(n) + ' is triangular!'
 	return (math.sqrt((8 * n) + 1) - 1) % 2 == 0
 
 def isPentagonal(n):
@@ -22,11 +11,13 @@ def isPentagonal(n):
 def getHexagonal(n):
 	return n * (2 * n - 1)
 
+def findNextHexaPentaTriangle(i):
+	while(True):
+		i+=1
+		hexagon = getHexagonal(i)
+		if isPentagonal(hexagon):
+			if isTriangular(hexagon):
+				print str(hexagon)
+				return
 
-i = 143
-while(True):
-	i+=1
-	hexagon = getHexagonal(i)
-	if isPentagonal(hexagon):
-		if isTriangular(hexagon):
-			print str(hexagon) + ' yay!'
+findNextHexaPentaTriangle(143)
