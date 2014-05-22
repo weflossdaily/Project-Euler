61dde708c065e4bc13e35885b5939de1f7c8f19d
weflossdaily <weflossdaily@gmail.com>
2014-05-22 19:19:28 -0400

    Create 048.py

---
+++ 048.py
@@ -0,0 +1,15 @@
+import sys
+
+numDigits = 10**10
+
+def getLastTenOfProduct(i):
+	product=1
+	for power in range(1,i+1):
+		product *= (i)
+		product = product % numDigits
+	return product
+
+sum=0
+for i in range(1,1001):
+	sum += getLastTenOfProduct(i)
+print sum % numDigits
