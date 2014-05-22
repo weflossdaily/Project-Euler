70f4eb29830da85be5fff4953ddf04128e4afdf3
weflossdaily <weflossdaily@gmail.com>
2014-05-22 19:59:33 -0400

    Create 092.py

---
+++ 092.py
@@ -0,0 +1,31 @@
+def endsAt89(n,targets,misses):
+    while(True):
+        #print str(n)
+        if n in targets:
+            return True
+        elif n in misses:
+            return False
+        else:
+            n=getNextDigit(n)
+        
+def getNextDigit(n):
+    sum=0
+    for char in str(n):
+        sum+=(int(char))**2
+    return sum
+
+total=0
+targets=set([89])
+misses=set([1])
+for i in range(1,567):
+    if endsAt89(i,targets,misses):
+        targets.add(i)
+        #print str(i) + ' ends at 89'
+        total+=1
+    else:
+        misses.add(i)
+for i in range(567,100000):
+    if endsAt89(i,targets,misses):
+        #print str(i) + ' ends at 89'
+        total+=1
+print str(total)
