ee5fb9122e4715495495ea2932eb1cc0ab545067
weflossdaily <rpowell@fishlos-Mini-2.local>
2014-06-03 22:31:25 -0400

    18 and 67

---
+++ 067.py
@@ -0,0 +1,17 @@
+cost = [0]
+inFile = open("067.in", "rw+")
+for line in inFile:
+	# print "cost: " + str(cost)
+	i = 0
+	newCost = []
+	nodes = line.split()
+	for node in nodes:
+		if i == 0:
+			newCost.append(cost[i] + int(nodes[i]))
+		elif i == len(nodes) - 1:
+			newCost.append(cost[len(cost) - 1] + int(nodes[i]))
+		else:
+			newCost.append(max(cost[i-1],cost[i]) + int(nodes[i]))
+		i += 1
+	cost = newCost
+print max(cost)
