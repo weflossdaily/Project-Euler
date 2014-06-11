49ffca02ca9b183aa642de1898b7a0d31ee70a50
weflossdaily <rpowell@fishlos-Mini-2.local>
2014-06-10 21:48:51 -0400

    113 is still no good

--- 113.py
+++ 113.py
@@ -9,5 +9,16 @@ def countDescending(prefix,numDigits):
 		return int(prefix[len(prefix) - 1]) + 1
 	return count
 
-digits = 7
-print 2 * countDescending('9',digits) - 10 - 1
+def countAscending(prefix,numDigits):
+	count = 0
+	if numDigits > 1:
+		for i in range(int(prefix[len(prefix) - 1]),10):
+			count += countAscending(prefix + str(i),numDigits - 1)
+	else:
+		#for i in range(int(prefix[len(prefix) - 1]),10):
+		#	print prefix + str(i)
+		return len(range(int(prefix[len(prefix) - 1]),10))
+	return count
+
+digits = 2
+print countDescending('9',digits) + countAscending('0',digits)
