a3bed3739241eb6ca1901cc341f43ade41fe52d3
weflossdaily <weflossdaily@gmail.com>
2014-06-13 15:43:04 -0400

    Update 035.py
    Eratosthenes update

--- 035.py
+++ 035.py
@@ -1,35 +1,64 @@
+thisMany = 1000000+1
+
+def unprimeIndices(number,primality):
+    leng = thisMany
+    currentIndexToUnprime=2*number 
+    while currentIndexToUnprime < leng:
+        primality[currentIndexToUnprime]=0
+        currentIndexToUnprime+=number 
+
+def findNextPrime(lastPrimeIndex,primality):
+    leng = thisMany
+    currentIndex=lastPrimeIndex+1
+    while currentIndex < leng and primality[currentIndex]==0:
+        currentIndex+=1
+    return currentIndex
+
+def printPrimes(primality):
+    for i in range(2,thisMany):
+        if primality[i]==1:
+            print str(i) + ', '
+
+def sumPrimes(primality):
+    sum=0
+    for i in range(2,thisMany):
+        if primality[i]==1:
+            sum+=i 
+    print str(sum)
+
 def isPrime(number,primes):
-	for prime in primes:
-		if number % prime == 0:
-			# print str(number) + " is divisible by " + str(prime)
-			return False
-	primes.append(number)
-	return True
+	if primes[number] == 1:
+		return True
+	else:
+		return False
 
 def permuteNumString(numString):
 	return numString[len(numString)-1] + numString[:-1]
 
 def isCircularPrime(number,primes):
-	numString = strNumber
+	numString = str(number)
 	if isPrime(number,primes):
 		newString = permuteNumString(numString)
 		while newString != numString:
-			if not isPrime(int(newString)):
+			if not isPrime(int(newString),primes):
 				return False
-			newString = permuteNumString(numString)
+			newString = permuteNumString(newString)
 	else:
 		return False
 	return True
 
-
-primes = []
+primalities=[1]*thisMany
+#why len give int not callable error
+leng= thisMany
+currentPrime=2
+while currentPrime < leng:
+    unprimeIndices(currentPrime,primalities)
+    currentPrime=findNextPrime(currentPrime,primalities)
+print 'got primes'
 count = 0
 # get primes, since current function requires all earlier primes
 for i in range(2,1000001):
-	isPrime(i,primes)
-print 'got primes'
-for i in range(2,1000001):
-	if isCircularPrime(i,primes):
+	if isCircularPrime(i,primalities):
 		print i
 		count += 1
 print count
