4dceaa9f9e8d5b8ba260d8f21b553bdcf43ebdcd
weflossdaily <weflossdaily@gmail.com>
2014-06-13 13:58:46 -0400

    Update 036.py
    add conversion

--- 036.py
+++ 036.py
@@ -7,7 +7,29 @@ def isPalindromic(numberString):
 			return False
 	return True
 
+def getHighestPowerOf2(number):
+	exponent = 0
+	product = 2**exponent
+	while True:
+		if number == product:
+			return exponent
+		elif product > number:
+			return (exponent - 1)
+		else:
+			exponent += 1
+			product *= 2
+
 def convertBase10To2(number):
+	conversion = ''
+	for exponent in reversed(range(getHighestPowerOf2(number)+1)):
+		exponentiation = 2**exponent
+		if number >= exponentiation:
+			conversion += '1'
+			number -= exponentiation
+			exponentiation /= 2
+		else:
+			conversion += '0'
+	return conversion
 
 total = 0
 i = 0
