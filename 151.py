9ad3b716540d423c521589901efe94a9229af50a
Reid <weflossdaily@gmail.com>
2015-12-30 15:30:56 -0500

    Update 151 with recursion
    needs work

--- 151.py
+++ 151.py
@@ -9,3 +9,26 @@ def harvestAttempt(sheetsInHand):
         sheetsInHand.remove(smallestSheet)
         oneHalf = smallestSheet + 1
         return harvestAttempt(sheetsInHand + [oneHalf,oneHalf])
+
+expectation = [0]
+def recursion(inventory,numLuckyPulls,parentProbability,expectation):
+    
+    if inventory == [5]:
+        return ((numLuckyPulls + len(inventory) - 1) * parentProbability)
+    
+    if len(inventory) == 1:
+        expectation[0] += parentProbability
+    
+    expectedPulls = 0
+    for sheetSize in inventory: #set(inventory):
+        #probability = inventory.count(sheetSize)/len(inventory)
+        probability = 1/len(inventory)
+        # print("Suppose that I pulled, with probability",probability,", a sheet size of",sheetSize,", having",numLuckyPulls,"lucky pulls so far with a total probability of",parentProbability * probability)
+        inventory.remove(sheetSize)
+        if sheetSize == 5:
+            # print(inventory)
+            expectedPulls += recursion(inventory, numLuckyPulls + 1, parentProbability * probability,expectation)
+        else:
+            # print(inventory + cutToSizeTheSmallestOf([sheetSize]))
+            expectedPulls += recursion(inventory + harvestAttempt([sheetSize]), numLuckyPulls, parentProbability * probability,expectation)
+    return expectedPulls
